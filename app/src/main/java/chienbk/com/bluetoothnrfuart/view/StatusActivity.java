8e21fae28bb3d7378e96b30224e41fdbd6ebc636
ChienNV9 <chiennv9@fsoft.com.vn>
2017-12-21 16:55:25 +0700

    Update icon and code

--- app/src/main/java/chienbk/com/bluetoothnrfuart/view/StatusActivity.java
+++ app/src/main/java/chienbk/com/bluetoothnrfuart/view/StatusActivity.java
@@ -5,11 +5,21 @@ import android.bluetooth.BluetoothDevice;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import chienbk.com.bluetoothnrfuart.R;
 
 public class StatusActivity extends AppCompatActivity {
+
+    private TextView txtVehicleSpeed, txtEngineRPM, txtEngineCoolant, txtFuelTank;
+    private EditText editMessage;
+    private ListView lvMessage;
+    private Button btnSendMessage;
 
     private BluetoothDevice mDevice = null;
     @Override
@@ -28,6 +38,32 @@ public class StatusActivity extends AppCompatActivity {
 
         showMessage(mDevice.getName());
 
+        initViews();
+        initControls();
+
+    }
+
+    private void initControls() {
+        //// TODO: 12/21/2017
+
+        btnSendMessage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showMessage("Send message");
+            }
+        });
+    }
+
+    private void initViews() {
+        txtVehicleSpeed = (TextView) findViewById(R.id.txt_vehicleSpeed);
+        txtEngineRPM = (TextView) findViewById(R.id.txt_engineRPM);
+        txtEngineCoolant = (TextView) findViewById(R.id.txt_engineCoolant);
+        txtFuelTank = (TextView) findViewById(R.id.txt_fuelTank);
+
+        editMessage = (EditText) findViewById(R.id.sendText);
+        lvMessage = (ListView) findViewById(R.id.listMessage);
+
+        btnSendMessage = (Button) findViewById(R.id.sendButton);
     }
 
     private void showMessage(String msg){
